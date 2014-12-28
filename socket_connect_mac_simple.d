b2b1a0b5043a3a3b3674fe22b529ac1e3702c0bc
Roberto Nibali <moreaki@Robertos-MacBook-Pro.local>
2014-12-28 04:15:25 +0100

    remove connect syscall, since it does not seem to exist in Yosemite

--- socket_connect_mac_simple.d
+++ socket_connect_mac_simple.d
@@ -5,7 +5,7 @@
 #pragma D option switchrate=1s
 #pragma D option destructive 
 
-inline int af_inet = 2;		/* AF_INET defined in bsd/sys/socket.h */
+inline int af_inet = 2;		/* AF_INET defined in /usr/include/sys/socket.h */
 
 dtrace:::BEGIN
 {
@@ -25,75 +25,6 @@ dtrace:::BEGIN
 
 	printf("%-6s %-16s %-3s %-16s %-5s %8s %s\n", "PID", "PROCESS", "FAM",
 	    "ADDRESS", "PORT", "LAT(us)", "RESULT");
-}
-
-/* MacOSX 10.10: 
-	connect(int socket, const struct sockaddr *address, socklen_t address_len); 
-	struct sockaddr {
-		__uint8_t	sa_len;	
-		sa_family_t	sa_family;	
-		char		sa_data[14];	
-	};
-*/
-
-syscall::connect:entry
-{
-    self->arg0 = arg0; /* int socket */
-    self->arg1 = arg1; /* const struct sockaddr *address */
-    self->arg2 = arg2; /* socklen_t address_len */
-    self->in = 1;
-}
-
-syscall::connect:entry
-/ self->in = 1 && execname == "nc" /
-{
-	printf("ENTERING /execname == nc/ [ENTRY] with %s\n", execname);
-	/*
-	trace(execname);
-	ustack();
-	*/
-		
-	this->len = *(socklen_t *) copyin((uintptr_t)self->arg2, sizeof(socklen_t));
-    this->socks = (struct sockaddr *) copyin((uintptr_t)self->arg1, this->len);
-
-	self->family = this->socks->sa_family;
-
-    this->hport = (uint_t)(this->socks->sa_data[0]);
-    this->lport = (uint_t)(this->socks->sa_data[1]);
-    this->hport <<= 8;
-
-    self->port = this->hport + this->lport;
-
-    this->a1 = lltostr((uint_t)this->socks->sa_data[2]);
-    this->a2 = lltostr((uint_t)this->socks->sa_data[3]);
-    this->a3 = lltostr((uint_t)this->socks->sa_data[4]);
-    this->a4 = lltostr((uint_t)this->socks->sa_data[5]);
-    this->s1 = strjoin(this->a1, ".");
-    this->s2 = strjoin(this->s1, this->a2);
-    this->s1 = strjoin(this->s2, ".");
-    this->s2 = strjoin(this->s1, this->a3);
-    this->s1 = strjoin(this->s2, ".");
-
-    self->address = strjoin(this->s1, this->a4);
-
-	self->start = timestamp;
-}
-
-syscall::connect:return
-/* /self->start/ */
-/execname == "nc" /
-{
-	printf("ENTERING /execname == nc/ [RETURN] with %s\n", execname);
-	this->delta = (timestamp - self->start) / 1000;
-	this->errstr = err[errno] != NULL ? err[errno] : lltostr(errno);
-	
-	printf("%-6d %-16s %-3d %-16s %-5d %8d %s\n", pid, execname, self->family, self->address, self->port, this->delta, this->errstr);
-	self->family = 0;
-	self->address = 0;
-	self->port = 0;
-	self->start = 0;
-	system("date");
-	/* system("/usr/bin/printf \"%s by %s in %d ms\"", probefunc, self->caller, this->delta); */
 }
 
 syscall::connectx:entry {
