40c54878ad81fd521ee4a907fe5b8358010e455c
Roberto Nibali <rnibali@gmail.com>
2014-12-28 23:46:03 +0100

    Reformatted for superuser question on stackoverflow: http://superuser.com/questions/815298/how-to-run-a-bash-script-in-response-to-a-network-event/857627#857627

--- socket_connect_mac_simple.d
+++ socket_connect_mac_simple.d
@@ -4,7 +4,7 @@
 #pragma D option switchrate=10hz
 #pragma D option destructive 
 
-/* AF_INET{6} are unknown to dtrace, so replace with numbers from /usr/include/sys/socket.h */
+/* AF_INET{6} are unknown to dtrace, so replace them with numbers */
 inline int af_inet  =  2; /* AF_INET  */
 inline int af_inet6 = 30; /* AF_INET6 */
 inline const string procname = "nc";
@@ -27,14 +27,16 @@ dtrace:::BEGIN
 	err[EADDRNOTAVAIL] = "Can't assign requested address";
 
 	printf("%-6s %-20s %-8s %-21s %-21s %-8s %s\n", 
-		"PID", "PROCNAME", "FAMILY", "S_ADDR:S_PORT", "D_ADDR:D_PORT", "LAT(us)", "RESULT");
+		"PID", "PROCNAME", "FAMILY", "S_ADDR:S_PORT", "D_ADDR:D_PORT", 
+		"LAT(us)", "RESULT");
 }
 
 /*  MacOSX connectx() syscall:
 
 	connectx(arg0:int s, arg1:struct sockaddr *src, arg2:socklen_t srclen, 
 						 arg3:struct sockaddr *dsts, arg4:socklen_t dstlen, 
-		 				 arg5:uint32_t ifscope, arg6: associd_t aid, arg7:connid_t *cid);
+		 				 arg5:uint32_t ifscope, arg6: associd_t aid, 
+						 arg7:connid_t *cid);
 */
 
 syscall::connectx:entry
@@ -84,7 +86,8 @@ syscall::connectx:return
 	/* Basically anything can be called here */
 	/* system("date"); */
 	printf("%-6d %-20s %-8d %-21s %-21s %-8d %s\n", 
-		pid, execname, this->f, self->s_addr, self->d_addr, this->delta, this->errstr); 
+		pid, execname, this->f, self->s_addr, self->d_addr, 
+		this->delta, this->errstr); 
 	
 	self->family = 0; 
 	self->ts = 0; 
